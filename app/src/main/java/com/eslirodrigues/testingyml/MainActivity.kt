1ff071a52608e9f951b34738c078c527d51c26bd
Eslicdm <eslicdm1@gmail.com>
2021-12-17 17:45:39 -0300

    Initial commit

---
+++ app/src/main/java/com/eslirodrigues/testingyml/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.eslirodrigues.testingyml
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.eslirodrigues.testingyml.ui.theme.TestingYmlTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TestingYmlTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    TestingYmlTheme {
+        Greeting("Android")
+    }
+}
